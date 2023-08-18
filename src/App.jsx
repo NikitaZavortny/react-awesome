cc05f7818e29eac9072506f9c6f81c9a07d8a06b
Nikita Zavorotnyi <zavorotnyni@gmail.com>
2023-08-18 17:28:09 +0800

    Initial commit for three.js lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import  Home  from './awesome-templates/home-template.jsx';
-import HomeFooter from './awesome-components/footers/footer-home.jsx';
-import MiniFooter from './awesome-components/footers/footer-mini.jsx';
-import CurveTransitionTemplate from './awesome-templates/curve-transition-template.jsx';
 
 import "./bootstrap.css";
-import "./curves.css";
 
 function App() {
   return (
@@ -13,12 +8,7 @@ function App() {
 
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" />
       <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
-      
-      <Home>
-        <h1>This is an design react project :D</h1>
-        <CurveTransitionTemplate />
-      </Home>
-      <HomeFooter/>
+      Hi there :D
     </div>
   );
 }
